7174fd9e86a2963705c1e6e712cc880a2b6e0056
孙昊翔 <673686754@qq.com>
2023-04-26 10:07:34 +0800

    chore: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@antfu/eslint-config-react'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn'
   },
 }
