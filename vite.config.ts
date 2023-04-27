8dbc835b3a3db1a28b1376a0bd24280330a4f494
孙昊翔 <673686754@qq.com>
2023-04-27 20:57:47 +0800

    chore: resolved the error when importing tsx using alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-// import { resolve } from 'path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// const res = path => resolve(__dirname, path)
+const res = path => resolve(__dirname, path)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     '@': res('src'),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      '@': res('src'),
+    },
+  },
 })
