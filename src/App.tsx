fb22609cf6172398ae9b4b9f65f16d5a04bb4a34
孙昊翔 <673686754@qq.com>
2023-04-28 07:04:41 +0800

    chore: path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Button, { ButtonType, ButtonSize } from './components/Button/button.tsx'
+import Button, {ButtonSize, ButtonType} from '@/components/Button/button.tsx'
 
 function App() {
   return (
     <>
-      <Button btnType={ButtonType.Danger}>Danger</Button>
-      <Button btnType={ButtonType.Link} href="www">Link</Button>
+      <Button btnType={ButtonType.Primary}>Primary</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger</Button>
+      <Button btnType={ButtonType.Link} href="https://www.google.com/">Google</Button>
+      <Button btnType={ButtonType.Link} href="https://www.google.com/" disabled>Disabled</Button>
     </>
   )
 }
