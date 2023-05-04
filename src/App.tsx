5cdad168f274f8dee6a06223dc9a0c24541c5362
孙昊翔 <673686754@qq.com>
2023-05-04 19:51:26 +0800

    feat: button style

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import Button, {ButtonSize, ButtonType} from '@/components/Button/button.tsx'
 function App() {
   return (
     <>
-      <Button btnType={ButtonType.Primary}>Primary</Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger</Button>
+      <Button>Default</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Danger</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/">Google</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/" disabled>Disabled</Button>
     </>
