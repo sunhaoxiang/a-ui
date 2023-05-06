d6ccba779a454833bad55aadb175a373e209289a
孙昊翔 <673686754@qq.com>
2023-05-06 16:09:44 +0800

    test: update button.test.tsx

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,8 +1,28 @@
 import { render } from '@/utils/test-utils'
 import Button from './button'
+import {describe, expect} from "vitest";
 
-test('our first react test case', () => {
-  const wrapper = render(<Button>Nice</Button>)
-  const element = wrapper.queryByText('Nice')
-  expect(element).toBeTruthy()
+// test('our first react test case', () => {
+//   const wrapper = render(<Button>Nice</Button>)
+//   const element = wrapper.queryByText('Nice')
+//   expect(element).toBeTruthy()
+//   expect(element).toBeInTheDocument()
+// })
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+
+  })
+
+  it('should render the correct component based on different props', () => {
+
+  })
+
+  it('should render a link when btnType equals link and href is provided', () => {
+
+  })
+
+  it('should render disabled button when disabled set to true', () => {
+
+  })
 })
