5738945d5152a8d4262d718d2ee8b7837d8368cc
孙昊翔 <673686754@qq.com>
2023-05-06 16:32:08 +0800

    test: update button.test.tsx

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,17 +1,14 @@
 import { render } from '@/utils/test-utils'
 import Button from './button'
-import {describe, expect} from "vitest";
-
-// test('our first react test case', () => {
-//   const wrapper = render(<Button>Nice</Button>)
-//   const element = wrapper.queryByText('Nice')
-//   expect(element).toBeTruthy()
-//   expect(element).toBeInTheDocument()
-// })
+import {describe, expect} from 'vitest'
 
 describe('test Button component', () => {
   it('should render the correct default button', () => {
-
+    const wrapper = render(<Button>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
   })
 
   it('should render the correct component based on different props', () => {
