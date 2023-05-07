37e70a6cae69291424e78436c420019512a05316
sunhaoxiang <673686754@qq.com>
2023-05-07 21:45:36 +0800

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Button, {ButtonSize, ButtonType} from '@/components/Button/button.tsx'
+import Menu from '@/components/Menu/menu.tsx'
+import MenuItem from '@/components/Menu/menuItem.tsx'
 
 const App = () => {
   return (
     <>
+      <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+        <MenuItem index={0}>cool link</MenuItem>
+        <MenuItem index={1} disabled>cool link 2</MenuItem>
+        <MenuItem index={2}>cool link 3</MenuItem>
+      </Menu>
       <Button autoFocus>Default</Button>
       <Button
         btnType={ButtonType.Danger}
@@ -14,6 +21,7 @@ const App = () => {
       <Button className="custom" btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/" target="_blank">Google</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/" disabled>Disabled</Button>
+
     </>
   )
 }
