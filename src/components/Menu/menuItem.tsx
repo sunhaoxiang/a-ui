5077d14b37a969f170c14f77c904bd6ea4176945
sunhaoxiang <673686754@qq.com>
2023-05-07 17:27:31 +0800

    feat: add props logic to MenuItem component

--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,6 +1,34 @@
-const MenuItem = () => {
+import Classnames from 'classnames'
+import type { CSSProperties, ReactNode } from 'react'
+
+export interface MenuItemProps {
+  index?: number
+  disabled?: boolean
+  className?: string
+  style?: CSSProperties
+  children: ReactNode
+}
+
+const MenuItem = (props: MenuItemProps) => {
+  const {
+    index,
+    disabled,
+    className,
+    style,
+    children,
+  } = props
+
+  const classes = Classnames('menu-item', className, {
+    'is-disabled': disabled,
+  })
+
   return (
-    <li></li>
+    <li
+      className={classes}
+      style={style}
+    >
+      {children}
+    </li>
   )
 }
 
