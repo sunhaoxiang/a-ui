b652213103250be3f1d46bf29afe21a0e86d2786
sunhaoxiang <673686754@qq.com>
2023-05-07 17:12:53 +0800

    refactor: Add props logic to Menu component

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
 import Classnames from 'classnames'
+import type { CSSProperties, ReactNode } from 'react'
 
-const Menu = () => {
+type MenuMode = 'horizontal' | 'vertical'
+
+export interface MenuProps {
+  defaultIndex?: number
+  className?: string
+  mode?: MenuMode
+  style?: CSSProperties,
+  onSelect?: (selectedIndex: number) => void,
+  children: ReactNode
+}
+
+const Menu = (props: MenuProps) => {
+  const {
+    defaultIndex,
+    className,
+    mode,
+    style,
+    onSelect,
+    children,
+  } = props
+
+  const classes = Classnames('menu', className, {
+    'menu-vertical': mode === 'vertical',
+  })
+
   return (
-    <div>Menu</div>
+    <ul
+      className={classes}
+      style={style}
+    >
+      {children}
+    </ul>
   )
+}
+
+Menu.defaultProps = {
+  defaultIndex: 0,
+  mode: 'horizontal',
 }
 
 export default Menu
