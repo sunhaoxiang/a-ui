373397aa67fb7a08306b9cbd31455c8a760c2d4e
sunhaoxiang <673686754@qq.com>
2023-05-08 20:34:15 +0800

    test: update button.test.tsx

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, vi } from 'vitest'
-import type { ButtonProps } from './button'
 import Button, { ButtonSize, ButtonType } from './button'
+import type { ButtonProps } from './button'
 import { fireEvent, render, screen } from '@/utils/test-utils'
 
 const defaultProps = {
@@ -21,7 +21,7 @@ const disabledProps: ButtonProps = {
 describe('test Button component', () => {
   it('should render the correct default button', () => {
     render(<Button {...defaultProps}>A Button</Button>)
-    const element = screen.getByRole('button', { name: /A Button/i })
+    const element = screen.getByRole('button')
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('BUTTON')
     expect(element).toHaveClass('btn btn-default')
@@ -33,14 +33,14 @@ describe('test Button component', () => {
 
   it('should render the correct component based on different props', () => {
     render(<Button {...testProps}>A Button</Button>)
-    const element = screen.getByRole('button', { name: /A Button/i })
+    const element = screen.getByRole('button')
     expect(element).toBeInTheDocument()
     expect(element).toHaveClass('btn-primary btn-lg klass')
   })
 
   it('should render a link when btnType equals link and href is provided', () => {
     render(<Button btnType={ButtonType.Link} href="http://dummyurl">A Link</Button>)
-    const element = screen.getByRole('link', { name: /A Link/i })
+    const element = screen.getByRole('link')
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('A')
     expect(element).toHaveClass('btn btn-link')
@@ -48,7 +48,7 @@ describe('test Button component', () => {
 
   it('should render disabled button when disabled set to true', () => {
     render(<Button {...disabledProps}>A Button</Button>)
-    const element = screen.getByRole('button', { name: /A Button/i })
+    const element = screen.getByRole('button')
     expect(element).toBeInTheDocument()
     expect(element).toBeDisabled()
     fireEvent.click(element)
