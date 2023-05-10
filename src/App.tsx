8d3a2a3908ddc68ce9145c0c10df64de0d83f292
sunhx <673686754@qq.com>
2023-05-10 20:54:02 +0800

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import Button, { ButtonSize, ButtonType } from '@/components/Button/button.tsx'
 import Menu from '@/components/Menu/menu.tsx'
+import SubMenu from '@/components/Menu/subMenu.tsx'
 import MenuItem from '@/components/Menu/menuItem.tsx'
 
 const App = () => {
   return (
     <>
-      <Menu defaultIndex={0} onSelect={(index) => { console.log(index) }}>
-        <MenuItem index={0}>cool link</MenuItem>
-        <MenuItem index={1} disabled>cool link 2</MenuItem>
-        <MenuItem index={2}>cool link 3</MenuItem>
+      <Menu defaultIndex="0" onSelect={(index) => { console.log(index) }}>
+        <MenuItem index="0">cool link</MenuItem>
+        <MenuItem index="1" disabled>cool link 2</MenuItem>
+        <MenuItem index="2">cool link 3</MenuItem>
+        <SubMenu index="3" title="dropDown">
+          <MenuItem index="3-0">dropdown 1</MenuItem>
+          <MenuItem index="3-0">dropdown 2</MenuItem>
+        </SubMenu>
       </Menu>
       <Button autoFocus>Default</Button>
       <Button
