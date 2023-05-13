f50698821661f92335a04a22dea9a295bd534981
sunhx <673686754@qq.com>
2023-05-13 16:59:19 +0800

    feat: add props logic to Icon component

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,6 +1,34 @@
-const Icon = () => {
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classnames from 'classnames'
+import type { FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+
+library.add(fas)
+
+export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
+
+export interface IconProps extends FontAwesomeIconProps {
+  theme?: ThemeProps
+  className?: string
+}
+
+const Icon = (props: IconProps) => {
+  const {
+    theme,
+    className,
+    ...restProps
+  } = props
+
+  const classes = classnames('icon', className, {
+    [`icon-${theme}`]: theme,
+  })
+
   return (
-    <div>Icon</div>
+    <FontAwesomeIcon
+      className={classes}
+      {...restProps}
+    />
   )
 }
 
