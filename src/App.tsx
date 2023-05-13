f522ee1de9df89ae0e41d7cff2fff692ea0c8b57
sunhx <673686754@qq.com>
2023-05-13 22:52:51 +0800

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import Button, { ButtonSize, ButtonType } from '@/components/Button/button.tsx'
 import Menu from '@/components/Menu/menu.tsx'
 import SubMenu from '@/components/Menu/subMenu.tsx'
 import MenuItem from '@/components/Menu/menuItem.tsx'
+import Icon from '@/components/Icon/icon.tsx'
 
 const App = () => {
   return (
     <>
-      <Menu defaultIndex="0" onSelect={(index) => { console.log(index) }}>
+      <Icon
+        icon="coffee"
+        theme="danger"
+        size="2xl"
+      />
+      <Menu
+        defaultIndex="0"
+        defaultOpenSubMenus={['3']}
+        mode="vertical"
+        onSelect={(index) => { console.log(index) }}
+      >
         <MenuItem index="0">cool link</MenuItem>
         <MenuItem index="1" disabled>cool link 2</MenuItem>
         <MenuItem index="2">cool link 3</MenuItem>
@@ -26,7 +37,6 @@ const App = () => {
       <Button className="custom" btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/" target="_blank">Google</Button>
       <Button btnType={ButtonType.Link} href="https://www.google.com/" disabled>Disabled</Button>
-
     </>
   )
 }
