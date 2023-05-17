2895f4ebdadf2165c14d7811f39e8cc3f7dccf9d
sunhx <673686754@qq.com>
2023-05-17 21:32:30 +0800

    feat: add props type to Alert component

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,3 +1,17 @@
+import classNames from 'classnames'
+import Icon from '@/components/Icon/icon.tsx'
+import Transition from '@/components/Transition/transition.tsx'
+
+export type AlertType = 'success' | 'default' | 'danger' | 'warning'
+
+export interface AlertProps {
+  title: string
+  description?: string
+  type?: AlertType
+  closable?: boolean
+  onClose?: () => void
+}
+
 const Alert = () => {
   return <div>Alert</div>
 }
