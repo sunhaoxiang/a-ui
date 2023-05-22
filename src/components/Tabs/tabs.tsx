9f43dc681b918b10d8baab153833d2dc168644d1
sunhx <673686754@qq.com>
2023-05-22 19:22:12 +0800

    feat: add type and props to Tabs component

--- src/components/Tabs/tabs.tsx
+++ src/components/Tabs/tabs.tsx
@@ -1,4 +1,16 @@
-const Tabs = () => {
+import type { ReactNode } from 'react'
+
+export interface TabsProps {
+  defaultIndex?: number
+  className?: string
+  onSelect?: (selectedIndex: number) => void
+  type?: 'line' | 'card'
+  children?: ReactNode
+}
+
+const Tabs = (props: TabsProps) => {
+  const { defaultIndex, className, onSelect, children, type } = props
+
   return <div>Tabs</div>
 }
 
