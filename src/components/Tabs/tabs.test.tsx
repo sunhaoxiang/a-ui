1d0a10559988289b8f8aa02239bb801e24d1901c
sunhx <673686754@qq.com>
2023-05-23 21:40:18 +0800

    test: add tabs.test.tsx

---
+++ src/components/Tabs/tabs.test.tsx
@@ -0,0 +1,54 @@
+import { describe, expect, vi } from 'vitest'
+import { render, fireEvent, screen, RenderResult } from '@/utils/test-utils'
+import Tabs, { TabsProps } from './tabs.tsx'
+import TabItem from './tabItem.tsx'
+
+const testProps: TabsProps = {
+  defaultIndex: 1,
+  onSelect: vi.fn()
+}
+let wrapper: RenderResult
+describe('test Tabs Component', () => {
+  beforeEach(() => {
+    wrapper = render(
+      <Tabs {...testProps}>
+        <TabItem label="tab1">content1</TabItem>
+        <TabItem label="tab2">content2</TabItem>
+        <TabItem label="disabled" disabled>
+          content3
+        </TabItem>
+      </Tabs>
+    )
+  })
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+  it('should render the correct default Tabs', () => {
+    const { container } = wrapper
+    expect(container.querySelector('.tabs-nav')).toHaveClass('nav-line')
+    const activeElement = screen.queryByText('tab2')
+    expect(activeElement).toBeInTheDocument()
+    expect(activeElement).toHaveClass('is-active')
+    expect(screen.queryByText('tab1')).not.toHaveClass('is-active')
+    expect(screen.queryByText('content2')).toBeInTheDocument()
+    expect(screen.queryByText('content1')).not.toBeInTheDocument()
+  })
+
+  it('click tabItem should switch to content', () => {
+    const clickedElement = screen.getByText('tab1')
+    fireEvent.click(clickedElement)
+    expect(clickedElement).toHaveClass('is-active')
+    expect(screen.queryByText('tab2')).not.toHaveClass('is-active')
+    expect(screen.queryByText('content1')).toBeInTheDocument()
+    expect(screen.queryByText('content2')).not.toBeInTheDocument()
+    expect(testProps.onSelect).toHaveBeenCalledWith(0)
+  })
+
+  it('click disabled tabItem should not works', () => {
+    const disableElement = screen.getByText('disabled')
+    expect(disableElement).toHaveClass('disabled')
+    fireEvent.click(disableElement)
+    expect(disableElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).not.toHaveBeenCalled()
+  })
+})
