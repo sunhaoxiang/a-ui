7c06118ac24b04295c5694e3e2e89ac7ac6df76f
sunhx <673686754@qq.com>
2023-05-25 19:12:16 +0800

    feat: add StoryBook to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,28 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/prettier'
-  ],
-  overrides: [
-    {
-      files: ['**/*.cjs'],
-      env: {
-        node: true,
-      },
-    },
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier/prettier', 'plugin:storybook/recommended'],
+  overrides: [{
+    files: ['**/*.cjs'],
+    env: {
+      node: true
+    }
+  }],
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['package.json', 'package-lock.json', 'node_modules/'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   plugins: ['react-refresh', 'testing-library', 'jest-dom'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'off',
-    'no-trailing-spaces': ["error", { "skipBlankLines": true }],
-  },
-}
+    'no-trailing-spaces': ["error", {
+      "skipBlankLines": true
+    }]
+  }
+};
