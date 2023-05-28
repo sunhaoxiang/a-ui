a86e68ca95df63c16d16775c5c9d3b69dea41536
sunhx <673686754@qq.com>
2023-05-28 22:04:31 +0800

    feat: update icon stories

--- src/components/Icon/icon.stories.tsx
+++ src/components/Icon/icon.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Icon from './icon.tsx'
-import Button from '../Button/button.tsx'
 
 const iconMeta: Meta<typeof Icon> = {
   title: 'General/Icon',
@@ -12,37 +11,68 @@ export default iconMeta
 
 type Story = StoryObj<typeof Icon>
 
+const Template: Story = {
+  render: args => <Icon {...args} />
+}
+
 export const DefaultIcon: Story = {
-  render: args => (
-    <>
-      <Icon {...args} icon="check" size="3x" />
-      <Icon {...args} icon="times" size="3x" />
-      <Icon {...args} icon="anchor" size="3x" />
-      <Icon {...args} icon="trash" size="3x" />
-      <Button size="lg" btnType="primary">
-        <Icon {...args} icon="check" />
-        check
-      </Button>
-    </>
-  )
+  ...Template,
+  args: {
+    icon: 'check',
+    size: '3x'
+  }
 }
 
-export const DifferentThemeIcon: Story = {
-  render: args => (
-    <>
-      <Icon {...args} icon="check" size="3x" theme="success" />
-      <Icon {...args} icon="times" size="3x" theme="danger" />
-      <Icon {...args} icon="anchor" size="3x" theme="primary" />
-      <Icon {...args} icon="exclamation-circle" size="3x" theme="warning" />
-    </>
-  )
+export const SuccessIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'check',
+    size: '3x',
+    theme: 'success'
+  }
 }
 
-export const CustomColorIcon: Story = {
-  render: args => (
-    <>
-      <Icon {...args} icon="spinner" size="3x" theme="primary" spin />
-      <Icon {...args} icon="spinner" size="3x" theme="success" pulse />
-    </>
-  )
+export const DangerIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'times',
+    size: '3x',
+    theme: 'danger'
+  }
+}
+
+export const PrimaryIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'anchor',
+    size: '3x',
+    theme: 'primary'
+  }
+}
+
+export const WarningIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'exclamation-circle',
+    size: '3x',
+    theme: 'warning'
+  }
+}
+
+export const SpinIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'spinner',
+    size: '3x',
+    spin: true
+  }
+}
+
+export const PulseIcon: Story = {
+  ...Template,
+  args: {
+    icon: 'spinner',
+    size: '3x',
+    pulse: true
+  }
 }
