52a2e3641db57454b009c9a65f2751cf043aaaec
sunhx <673686754@qq.com>
2023-05-29 21:40:11 +0800

    feat: create input component

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,6 +1,18 @@
-import { FC } from 'react'
+import { FC, ReactElement, InputHTMLAttributes } from 'react'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
-export const Input: FC = () => {
+type InputSize = 'lg' | 'sm'
+
+export interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLElement>, 'size'> {
+  disabled?: boolean
+  size?: InputSize
+  icon?: IconProp
+  prepend?: string | ReactElement
+  append?: string | ReactElement
+}
+
+export const Input: FC<InputProps> = () => {
   return <div>input</div>
 }
 
