7f5e9d97999cb0f3009f13408809ec710bbf0e18
sunhx <673686754@qq.com>
2023-05-30 21:07:13 +0800

    style: code style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,21 @@ module.exports = {
     browser: true,
     es2020: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier/prettier', 'plugin:storybook/recommended'],
-  overrides: [{
-    files: ['**/*.cjs'],
-    env: {
-      node: true
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/prettier',
+    'plugin:storybook/recommended'
+  ],
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      env: {
+        node: true
+      }
     }
-  }],
+  ],
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['package.json', 'package-lock.json', 'node_modules/'],
   parserOptions: {
@@ -21,8 +29,11 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'off',
-    'no-trailing-spaces': ["error", {
-      "skipBlankLines": true
-    }]
+    'no-trailing-spaces': [
+      'error',
+      {
+        skipBlankLines: true
+      }
+    ]
   }
-};
+}
