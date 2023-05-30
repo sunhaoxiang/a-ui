b9978770fd93e5e0e8978dec957f452448416d4b
sunhx <673686754@qq.com>
2023-05-30 19:29:23 +0800

    feat: add Input component

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,4 +1,11 @@
-import { FC, ReactElement, InputHTMLAttributes } from 'react'
+import {
+  ReactElement,
+  InputHTMLAttributes,
+  ChangeEvent,
+  forwardRef
+} from 'react'
+import classNames from 'classnames'
+import Icon from '@/components/Icon/icon.tsx'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 type InputSize = 'lg' | 'sm'
@@ -10,10 +17,49 @@ export interface InputProps
   icon?: IconProp
   prepend?: string | ReactElement
   append?: string | ReactElement
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input: FC<InputProps> = () => {
-  return <div>input</div>
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { disabled, size, icon, prepend, append, style, ...restProps } = props
+
+  const cnames = classNames('input-wrapper', {
+    [`input-size-${size}`]: size,
+    'is-disabled': disabled,
+    'input-group': prepend || append,
+    'input-group-append': !!append,
+    'input-group-prepend': !!prepend
+  })
+
+  const fixControlledValue = (value: any) => {
+    if (typeof value === 'undefined' || value === null) {
+      return ''
+    }
+    return value
+  }
+
+  if ('value' in props) {
+    delete restProps.defaultValue
+    restProps.value = fixControlledValue(props.value)
+  }
+
+  return (
+    <div className={cnames} style={style}>
+      {prepend && <div className="input-group-prepend">{prepend}</div>}
+      {icon && (
+        <div className="icon-wrapper">
+          <Icon icon={icon} title={`title-${icon}`} />
+        </div>
+      )}
+      <input
+        ref={ref}
+        className="input-inner"
+        disabled={disabled}
+        {...restProps}
+      />
+      {append && <div className="input-group-append">{append}</div>}
+    </div>
+  )
+})
 
 export default Input
