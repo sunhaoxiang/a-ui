a1884a773e0ac72f67689f35d9095de8dfac56bb
sunhx <673686754@qq.com>
2023-05-30 21:26:18 +0800

    test: add input.test.tsx

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, vi } from 'vitest'
+import { fireEvent, render, screen } from '@/utils/test-utils'
+import { Input, InputProps } from './input'
+
+const defaultProps: InputProps = {
+  onChange: vi.fn(),
+  placeholder: 'test-input'
+}
+
+describe('test Input component', () => {
+  it('should render the correct default Input', () => {
+    render(<Input {...defaultProps} />)
+    const testNode = screen.getByPlaceholderText(
+      'test-input'
+    ) as HTMLInputElement
+    expect(testNode).toBeInTheDocument()
+    expect(testNode).toHaveClass('input-inner')
+    fireEvent.change(testNode, { target: { value: '123' } })
+    expect(defaultProps.onChange).toHaveBeenCalled()
+    expect(testNode.value).toEqual('123')
+  })
+
+  it('should render the disabled Input on disabled property', () => {
+    render(<Input disabled placeholder="disabled" />)
+    const testNode = screen.getByPlaceholderText('disabled')
+    expect(testNode).toBeDisabled()
+  })
+
+  it('should render different input sizes on size property', () => {
+    const { container } = render(<Input placeholder="sizes" size="lg" />)
+    const testContainer = container.querySelector('.input-wrapper')
+    expect(testContainer).toHaveClass('input-size-lg')
+  })
+
+  it('should render prepand and append element on prepand/append property', () => {
+    const { container } = render(
+      <Input placeholder="pend" prepend="https://" append=".com" />
+    )
+    const testContainer = container.querySelector('.input-wrapper')
+    expect(testContainer).toHaveClass(
+      'input-group input-group-append input-group-prepend'
+    )
+    expect(screen.queryByText('https://')).toBeInTheDocument()
+    expect(screen.queryByText('.com')).toBeInTheDocument()
+  })
+})
