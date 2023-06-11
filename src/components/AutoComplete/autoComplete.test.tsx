d16881c752b4fcbf1bbcdc028da07f61450a1dd8
sunhx <673686754@qq.com>
2023-06-11 11:36:51 +0800

    test: update autoComplete.test.tsx

--- src/components/AutoComplete/autoComplete.test.tsx
+++ src/components/AutoComplete/autoComplete.test.tsx
@@ -1,7 +1,17 @@
 import { beforeEach, describe, expect, vi } from 'vitest'
 import { config } from 'react-transition-group'
-import { AutoComplete, AutoCompleteProps } from './autoComplete.tsx'
-import { fireEvent, render, RenderResult, screen } from '@/utils/test-utils.tsx'
+import {
+  AutoComplete,
+  AutoCompleteProps,
+  DataSourceType
+} from './autoComplete.tsx'
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+  waitFor
+} from '@/utils/test-utils.tsx'
 
 config.disabled = true
 
@@ -12,12 +22,26 @@ const testArray = [
   { value: 'c', number: 15 }
 ]
 
+const renderOption = (item: DataSourceType) => {
+  const itemWithNumber = item as DataSourceType<{
+    value: string
+    number: number
+  }>
+  return <>name: {itemWithNumber.value}</>
+}
+
 const testProps: AutoCompleteProps = {
   fetchSuggestions: query => {
     return testArray.filter(item => item.value.includes(query))
   },
   onSelect: vi.fn(),
   placeholder: 'auto-complete'
+}
+
+const testPropsWithCustomRender: AutoCompleteProps = {
+  ...testProps,
+  placeholder: 'auto-complete-2',
+  renderOption
 }
 
 let wrapper: RenderResult, inputNode: HTMLInputElement
@@ -30,7 +54,6 @@ describe('test AutoComplete component', () => {
 
   it('test basic AutoComplete behavior', async () => {
     const { container } = wrapper
-    // input change
     fireEvent.change(inputNode, { target: { value: 'a' } })
     expect(await screen.findByText('ab')).toBeInTheDocument()
     expect(await screen.findByText('abc')).toBeInTheDocument()
@@ -40,29 +63,24 @@ describe('test AutoComplete component', () => {
     fireEvent.click(screen.getByText('ab'))
     expect(testProps.onSelect).toHaveBeenCalledWith({ value: 'ab', number: 11 })
     expect(screen.queryByText('ab')).not.toBeInTheDocument()
-    //fill the input
     expect(inputNode.value).toBe('ab')
   })
 
   it('should provide keyboard support', async () => {
-    // input change
     fireEvent.change(inputNode, { target: { value: 'a' } })
     expect(await screen.findByText('ab')).toBeInTheDocument()
     const firstResult = screen.queryByText('ab')
     const secondResult = screen.queryByText('abc')
-
     // press arrow down
     fireEvent.keyDown(inputNode, {
       code: 'ArrowDown'
     })
     expect(firstResult).toHaveClass('is-active')
-
     // press arrow down
     fireEvent.keyDown(inputNode, {
       code: 'ArrowDown'
     })
     expect(secondResult).toHaveClass('is-active')
-
     // press arrow up
     fireEvent.keyDown(inputNode, { code: 'ArrowUp' })
     expect(firstResult).toHaveClass('is-active')
@@ -70,5 +88,42 @@ describe('test AutoComplete component', () => {
     fireEvent.keyDown(inputNode, { code: 'Enter' })
     expect(testProps.onSelect).toHaveBeenCalledWith({ value: 'ab', number: 11 })
     expect(screen.queryByText('ab')).not.toBeInTheDocument()
+  })
+
+  it('click outside should hide the dropdown', async () => {
+    fireEvent.change(inputNode, { target: { value: 'a' } })
+    expect(await screen.findByText('ab')).toBeInTheDocument()
+    fireEvent.click(document)
+    expect(screen.queryByText('ab')).not.toBeInTheDocument()
+  })
+
+  it('renderOption should generate the right template', async () => {
+    render(<AutoComplete {...testPropsWithCustomRender} />)
+    const inputNode = screen.getByPlaceholderText(
+      'auto-complete-2'
+    ) as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: 'a' } })
+    expect(await screen.findByText('name: ab')).toBeInTheDocument()
+  })
+
+  it('async fetchSuggestions should works fine', async () => {
+    const testPropsWithPromise: AutoCompleteProps = {
+      ...testProps,
+      fetchSuggestions: vi.fn(query => {
+        return Promise.resolve(
+          testArray.filter(item => item.value.includes(query))
+        )
+      }),
+      placeholder: 'auto-complete-3'
+    }
+    render(<AutoComplete {...testPropsWithPromise} />)
+    const inputNode = screen.getByPlaceholderText(
+      'auto-complete-3'
+    ) as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: 'a' } })
+    await waitFor(() => {
+      expect(testPropsWithPromise.fetchSuggestions).toHaveBeenCalled()
+    })
+    expect(await screen.findByText('ab')).toBeInTheDocument()
   })
 })
