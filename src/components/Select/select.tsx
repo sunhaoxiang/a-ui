ee63302f1ec2bd87e50748b767398a8305da2d18
sunhx <673686754@qq.com>
2023-06-12 22:12:34 +0800

    feat: create Select component

---
+++ src/components/Select/select.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+
+export interface SelectProps {
+  defaultValue?: string | string[]
+}
+export const Select: FC<SelectProps> = () => {
+  return <div>Select</div>
+}
+
+export default Select
