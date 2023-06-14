00399864e82bf3e17c55e7e7de8d97e577169637
sunhx <673686754@qq.com>
2023-06-14 21:45:17 +0800

    feat: add select.stories.tsx

---
+++ src/components/Select/select.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Select from './select.tsx'
+import Option from './option.tsx'
+
+const selectMeta: Meta<typeof Select> = {
+  title: 'Data Entry/Select',
+  component: Select,
+  decorators: [
+    Story => (
+      <div style={{ width: '350px', paddingBottom: '200px' }}>
+        <Story />
+      </div>
+    )
+  ],
+  tags: ['autodocs']
+}
+
+export default selectMeta
+
+type Story = StoryObj<typeof Select>
+
+export const DefaultSelect: Story = {
+  render: args => (
+    <Select {...args} placeholder="Select">
+      <Option value="Jack" />
+      <Option value="Lucy" />
+      <Option value="Hello" />
+      <Option value="disabled" disabled />
+    </Select>
+  )
+}
+
+export const MultipleSelect: Story = {
+  render: args => (
+    <Select {...args} placeholder="Select" multiple>
+      <Option value="Jack" />
+      <Option value="Lucy" />
+      <Option value="Hello" />
+      <Option value="disabled" disabled />
+    </Select>
+  )
+}
+
+export const DisabledSelect: Story = {
+  render: args => (
+    <Select {...args} placeholder="Select" disabled>
+      <Option value="Jack" />
+      <Option value="Lucy" />
+      <Option value="Hello" />
+      <Option value="disabled" disabled />
+    </Select>
+  )
+}
