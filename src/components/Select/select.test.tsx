36767e2f5a7436709e473a7df4b990711a9b84bc
sunhx <673686754@qq.com>
2023-06-16 22:21:06 +0800

    test: update select.test.tsx

--- src/components/Select/select.test.tsx
+++ src/components/Select/select.test.tsx
@@ -1,4 +1,28 @@
-import { describe } from 'vitest'
+import { beforeEach, describe, expect, vi } from 'vitest'
+import { config } from 'react-transition-group'
+import Select, { SelectProps } from './select.tsx'
+import Option from './option.tsx'
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+  waitFor
+} from '@/utils/test-utils.tsx'
+
+config.disabled = true
+
+const testProps: SelectProps = {
+  defaultValue: '',
+  placeholder: 'test',
+  onChange: vi.fn(),
+  onVisibleChange: vi.fn()
+}
+
+const multipleProps: SelectProps = {
+  ...testProps,
+  multiple: true
+}
 
 describe('test Select component', () => {
   it('should render the correct Select component')
