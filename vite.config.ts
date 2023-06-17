29174dc48eacc87b5ee36337ab5f1860da665319
sunhx <673686754@qq.com>
2023-06-17 22:29:26 +0800

    style: code style

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 const res = path => resolve(__dirname, path)
 
 // https://vitejs.dev/config/
@@ -13,15 +12,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': res('src'),
-    },
+      '@': res('src')
+    }
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: './src/test/setup.ts'
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     // css: true,
-  },
+  }
 })
