5c17bb2989128fc7b7254b1046e3bc13f8f52bfe
sunhx <673686754@qq.com>
2023-06-17 23:27:37 +0800

    feat: create Upload component

---
+++ src/components/Upload/upload.tsx
@@ -0,0 +1,9 @@
+import { FC, useRef } from 'react'
+import axios from 'axios'
+import Button from '@/components/Button/button.tsx'
+
+export const Upload: FC = () => {
+  return <div className="a-upload">upload</div>
+}
+
+export default Upload
