5bb4f3a073b0a068d8f96ee953ded011b0369b5a
sunhx <673686754@qq.com>
2023-06-17 23:44:56 +0800

    feat: add props to Upload component

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -2,8 +2,24 @@ import { FC, useRef } from 'react'
 import axios from 'axios'
 import Button from '@/components/Button/button.tsx'
 
-export const Upload: FC = () => {
-  return <div className="a-upload">upload</div>
+export interface UploadProps {
+  action: string
+  onProgress?: (percentage: number, file: File) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onSuccess?: (data: any, file: File) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onError?: (err: any, file: File) => void
+}
+
+export const Upload: FC<UploadProps> = props => {
+  const { action, onProgress, onSuccess, onError } = props
+
+  return (
+    <div className="a-upload-component">
+      <Button btnType="primary">Upload File</Button>
+      <input className="a-file-input" style={{ display: 'none' }} type="file" />
+    </div>
+  )
 }
 
 export default Upload
