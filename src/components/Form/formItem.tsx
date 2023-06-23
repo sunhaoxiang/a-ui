929d9416eb9662b1882d6672e4a61a2164a1a840
sunhx <673686754@qq.com>
2023-06-23 11:23:56 +0800

    feat: use context and reducer to dispatch field data in formItem

--- src/components/Form/formItem.tsx
+++ src/components/Form/formItem.tsx
@@ -1,17 +1,33 @@
-import { FC, ReactNode } from 'react'
+import { useContext, useEffect, FC, ReactNode } from 'react'
 import classNames from 'classnames'
+import { FormContext } from './form.tsx'
 
 export interface FormItemProps {
+  name: string
   label?: string
   children?: ReactNode
 }
 
 export const FormItem: FC<FormItemProps> = props => {
-  const { label, children } = props
+  const { name, label, children } = props
+
+  const { dispatch } = useContext(FormContext)
 
   const rowClass = classNames('a-row', {
     'a-row-no-label': !label
   })
+
+  useEffect(() => {
+    dispatch({
+      type: 'addField',
+      name,
+      value: {
+        label,
+        name
+      }
+    })
+  }, [])
+
   return (
     <div className={rowClass}>
       {label && (
