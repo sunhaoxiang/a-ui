24e9124f530442cb8fcfbfd4097c4b590112f9ba
sunhx <673686754@qq.com>
2023-06-28 21:11:29 +0800

    test: add form.test.tsx

---
+++ src/components/Form/form.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest'
+
+describe('testing Form component', () => {
+  it('should render the correct Form component')
+
+  it('submit form with invalid values should show the error message')
+
+  it('change single input to invalid values should trigger the validate')
+
+  it('custom rules should work')
+})
