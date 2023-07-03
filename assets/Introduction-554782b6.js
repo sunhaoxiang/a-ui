534b225318382b71e096779bd5d45ea62998a817
sunhaoxiang <673686754@qq.com>
2023-07-03 11:40:04 +0000

    Deploying to gh-pages from @ sunhaoxiang/a-ui@735e2e77cfdc17467e4b42d07996c8e2f9c475d0 🚀

---
+++ assets/Introduction-554782b6.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-94f6e698.js";import{M as a}from"./index-15742cbe.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-3693928d.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-8ce4a492.js";import"./index-d37d4223.js";import"./inheritsLoose-9eefaa95.js";import"./index-6e6be2d5.js";import"./index-356e4a49.js";function o(e){const t=Object.assign({h2:"h2",pre:"pre",code:"code"},r(),e.components);return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Introduction"}),`
+`,n.jsx(t.h2,{id:"installation",children:"Installation"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-bash",children:`npm install @a-front-end-project/a-ui
+`})}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-bash",children:`yarn add @a-front-end-project/a-ui
+`})}),`
+`,n.jsx(t.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-jsx",children:`import React from 'react';
+import { Button } from '@a-front-end-project/a-ui';
+
+const App = () => (
+  <>
+    <Button btnType="primary">PRESS ME</Button>
+  </>
+);
+`})})]})}function M(e={}){const{wrapper:t}=Object.assign({},r(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(o,e)})):o(e)}export{M as default};
+//# sourceMappingURL=Introduction-554782b6.js.map
