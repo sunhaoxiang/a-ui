3f6a3db00cd15138b51ca389c58621b21f66e1d8
sunhaoxiang <673686754@qq.com>
2023-07-03 12:57:27 +0000

    Deploying to gh-pages from @ sunhaoxiang/a-ui@23b814aa80344c954a9250a8fc59f32b9d069106 🚀

--- assets/preview-603c5c1e.js
+++
@@ -1,2 +0,0 @@
-import{l as o,i as a}from"./index-c6694cd8.js";o.add(a);const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-603c5c1e.js.map
