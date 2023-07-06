13c986132584d5949f24744cadf8e23a86eefb5d
sunhx <673686754@qq.com>
2023-07-06 19:05:17 +0800

    docs: update Icon docs

--- src/components/Icon/icon.stories.tsx
+++ src/components/Icon/icon.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Icon from '@/components/Icon'
+import { parameters } from '@/utils/storybook-utils'
 
 const iconMeta: Meta<typeof Icon> = {
   title: 'General/Icon',
@@ -20,13 +21,30 @@ const Template: Story = {
   render: args => <Icon {...args} />
 }
 
+const defaultIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="check" size="3x" />
+)
+`
+
 export const DefaultIcon: Story = {
   ...Template,
   args: {
     icon: 'check',
     size: '3x'
-  }
+  },
+  parameters: parameters({ code: defaultIconCode })
 }
+
+const successIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="check" size="3x" theme="success" />
+)
+`
 
 export const SuccessIcon: Story = {
   ...Template,
@@ -34,8 +52,17 @@ export const SuccessIcon: Story = {
     icon: 'check',
     size: '3x',
     theme: 'success'
-  }
+  },
+  parameters: parameters({ code: successIconCode })
 }
+
+const dangerIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="times" size="3x" theme="danger" />
+)
+`
 
 export const DangerIcon: Story = {
   ...Template,
@@ -43,8 +70,17 @@ export const DangerIcon: Story = {
     icon: 'times',
     size: '3x',
     theme: 'danger'
-  }
+  },
+  parameters: parameters({ code: dangerIconCode })
 }
+
+const primaryIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="anchor" size="3x" theme="primary" />
+)
+`
 
 export const PrimaryIcon: Story = {
   ...Template,
@@ -52,8 +88,17 @@ export const PrimaryIcon: Story = {
     icon: 'anchor',
     size: '3x',
     theme: 'primary'
-  }
+  },
+  parameters: parameters({ code: primaryIconCode })
 }
+
+const warningIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="exclamation-circle" size="3x" theme="warning" />
+)
+`
 
 export const WarningIcon: Story = {
   ...Template,
@@ -61,8 +106,17 @@ export const WarningIcon: Story = {
     icon: 'exclamation-circle',
     size: '3x',
     theme: 'warning'
-  }
+  },
+  parameters: parameters({ code: warningIconCode })
 }
+
+const spinIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="spinner" size="3x" spin />
+)
+`
 
 export const SpinIcon: Story = {
   ...Template,
@@ -70,8 +124,17 @@ export const SpinIcon: Story = {
     icon: 'spinner',
     size: '3x',
     spin: true
-  }
+  },
+  parameters: parameters({ code: spinIconCode })
 }
+
+const pulseIconCode = `
+import { Icon } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Icon icon="spinner" size="3x" pulse />
+)
+`
 
 export const PulseIcon: Story = {
   ...Template,
@@ -79,5 +142,6 @@ export const PulseIcon: Story = {
     icon: 'spinner',
     size: '3x',
     pulse: true
-  }
+  },
+  parameters: parameters({ code: pulseIconCode })
 }
