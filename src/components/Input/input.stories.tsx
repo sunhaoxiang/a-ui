b98c60d15a3b03dc3a4dfa88b50b6f60272d516a
sunhx <673686754@qq.com>
2023-07-07 18:52:57 +0800

    docs: update Input Docs

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Input from './input'
+import { parameters } from '@/utils/storybook-utils.tsx'
 
 const inputMeta: Meta<typeof Input> = {
   title: 'Data Entry/Input',
@@ -10,8 +11,7 @@ const inputMeta: Meta<typeof Input> = {
         <Story />
       </div>
     )
-  ],
-  tags: ['autodocs']
+  ]
 }
 
 export default inputMeta
@@ -22,57 +22,120 @@ const Template: Story = {
   render: args => <Input {...args} />
 }
 
+const defaultInputCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input placeholder="Default Input" />
+)
+`
+
 export const DefaultInput: Story = {
   ...Template,
   args: {
     placeholder: 'Default Input'
-  }
+  },
+  parameters: parameters({ code: defaultInputCode })
 }
+
+const disabledInputCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input placeholder="Disabled Input" disabled />
+)
+`
 
 export const DisabledInput: Story = {
   ...Template,
   args: {
     placeholder: 'Disabled Input',
     disabled: true
-  }
+  },
+  parameters: parameters({ code: disabledInputCode })
 }
+
+const largeInputCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input size="lg" placeholder="Large Input" />
+)
+`
 
 export const LargeInput: Story = {
   ...Template,
   args: {
-    placeholder: 'large input',
+    placeholder: 'Large Input',
     size: 'lg'
-  }
+  },
+  parameters: parameters({ code: largeInputCode })
 }
+
+const smallInputCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input size="sm" placeholder="Small Input" />
+)
+`
 
 export const SmallInput: Story = {
   ...Template,
   args: {
-    placeholder: 'small input',
+    placeholder: 'Small Input',
     size: 'sm'
-  }
+  },
+  parameters: parameters({ code: smallInputCode })
 }
+
+const inputWithIconCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input icon="search" placeholder="Input With Icon" />
+)
+`
 
 export const InputWithIcon: Story = {
   ...Template,
   args: {
-    placeholder: 'input with icon',
+    placeholder: 'Input With Icon',
     icon: 'search'
-  }
+  },
+  parameters: parameters({ code: inputWithIconCode })
 }
+
+const inputWithPrependCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input prepend="https://" placeholder="google.com" />
+)
+`
 
 export const InputWithPrepend: Story = {
   ...Template,
   args: {
     placeholder: 'google.com',
     prepend: 'https://'
-  }
+  },
+  parameters: parameters({ code: inputWithPrependCode })
 }
+
+const inputWithAppendCode = `
+import { Input } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Input append=".com" placeholder="google" />
+)
+`
 
 export const InputWithAppend: Story = {
   ...Template,
   args: {
     placeholder: 'google',
     append: '.com'
-  }
+  },
+  parameters: parameters({ code: inputWithAppendCode })
 }
