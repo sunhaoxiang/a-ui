9d13f94de6162cc3e07407e4f37219f23736059f
sunhx <673686754@qq.com>
2023-07-07 20:07:00 +0800

    docs: update Alert Docs

--- src/components/Alert/alert.stories.tsx
+++ src/components/Alert/alert.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Alert from './alert'
+import { parameters } from '@/utils/storybook-utils'
 
 const alertMeta: Meta<typeof Alert> = {
   title: 'Feedback/Alert',
-  component: Alert,
-  tags: ['autodocs']
+  component: Alert
 }
 
 export default alertMeta
@@ -15,49 +15,130 @@ const Template: Story = {
   render: args => <Alert {...args} />
 }
 
+const defaultAlertCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert title="this is alert!" />
+)
+
+export default App
+`
+
 export const DefaultAlert: Story = {
   ...Template,
   args: {
     title: 'this is alert!'
-  }
+  },
+  parameters: parameters({ code: defaultAlertCode })
 }
+
+const alertWithDescriptionCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert
+   title="this is alert!"
+   description="this is description!"
+  />
+)
+
+export default App
+`
 
 export const AlertWithDescription: Story = {
   ...Template,
   args: {
     title: 'this is alert!',
     description: 'this is description!'
-  }
+  },
+  parameters: parameters({ code: alertWithDescriptionCode })
 }
+
+const alertCanNotCloseCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert
+   title="this is alert!"
+   closable={false}
+  />
+)
+
+export default App
+`
 
 export const AlertCanNotClose: Story = {
   ...Template,
   args: {
     title: 'this is alert!',
     closable: false
-  }
+  },
+  parameters: parameters({ code: alertCanNotCloseCode })
 }
+
+const successAlertCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert
+   title="this is success!"
+   type="success"
+  />
+)
+
+export default App
+`
 
 export const SuccessAlert: Story = {
   ...Template,
   args: {
     title: 'this is success!',
     type: 'success'
-  }
+  },
+  parameters: parameters({ code: successAlertCode })
 }
+
+const dangerAlertCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert
+   title="this is danger!"
+   type="danger"
+  />
+)
+
+export default App
+`
 
 export const DangerAlert: Story = {
   ...Template,
   args: {
     title: 'this is danger!',
     type: 'danger'
-  }
+  },
+  parameters: parameters({ code: dangerAlertCode })
 }
+
+const warningAlertCode = `
+import { Alert } from '@a-front-end-project/a-ui'
+
+const App = () => (
+  <Alert
+   title="this is warning!"
+   type="warning"
+  />
+)
+
+export default App
+`
 
 export const WarningAlert: Story = {
   ...Template,
   args: {
     title: 'this is warning!',
     type: 'warning'
-  }
+  },
+  parameters: parameters({ code: warningAlertCode })
 }
