6cba6e0f54e346bb630ac64847fb2b109bc3d623
sunhx <673686754@qq.com>
2023-07-10 19:52:41 +0800

    feat: update Form component types export

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,6 +3,15 @@ import Item from './formItem'
 import type { FC } from 'react'
 import type { RenderProps, FormProps, IFormContext, IFormRef } from './form'
 import type { FormItemProps } from './formItem'
+import type {
+  CustomRuleFunc,
+  CustomRule,
+  FieldDetail,
+  FieldsState,
+  ValidateErrorType,
+  FormState,
+  FieldAction
+} from './useStore'
 
 export type IFormComponent = typeof Form & {
   Item: FC<FormItemProps>
@@ -12,5 +21,18 @@ const TransForm: IFormComponent = Form as IFormComponent
 
 TransForm.Item = Item
 
-export type { RenderProps, FormProps, IFormContext, IFormRef, FormItemProps }
+export type {
+  RenderProps,
+  FormProps,
+  IFormContext,
+  IFormRef,
+  FormItemProps,
+  CustomRuleFunc,
+  CustomRule,
+  FieldDetail,
+  FieldsState,
+  ValidateErrorType,
+  FormState,
+  FieldAction
+}
 export default TransForm
